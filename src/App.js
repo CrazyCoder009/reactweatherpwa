44aa9ba598a61792d91ac7ca2919dbc236708bc4
RRN <rrn@pop-os.localdomain>
2020-12-19 00:09:22 +0530

    Day 2 commit -1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {fetchWeather} from './api/fetchWeather';
+import './App.css';
 
 const App = () => {
+    const [query, setQuery] = useState('');
+
+    const search = async(e)=>{
+        if(e.key === 'Enter'){
+            const data=await fetchWeather(query)
+            console.log(data);
+        }
+    }
     return(
-        <h1>Returned text 123</h1>
+        <div className="main-container">
+            <input
+            type="text"
+            className="Search"
+            placeholder="Search.... "
+            value={query}
+            onChange={(e)=> setQuery(e.target.value)}
+            onKeyPress={search}
+            />
+        </div>
     );
 }
 
